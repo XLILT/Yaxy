ca1ede62e16a46ae3d817a90353793b0b65d345c
maxiaolong <maxiaolong@sunlands.com>
2018-06-13 13:16:16 +0800

    test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
  var server = require('http').createServer(onRequest);
  server.on('connect', onConnect);
  server.listen(8080);
+ //server.listen(3002);
 
  function onRequest(req, res) {
     console.log(req)
